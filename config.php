a05b8b4414f967d9ad57744ac07dfd21aa57f9c5
aftabmogal <mogalaftab940@gmail.com>
2025-08-12 15:33:42 +0530

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Edit this file with your database credentials and save as config.php
-$db_host = 'localhost';
-$db_name = 'sk_auto_db';
-$db_user = 'sk_user';
-$db_pass = 'sk_password';
-?>
